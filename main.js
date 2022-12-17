7452b96ceec0c5f1f206969751e64ff3e8967736
DM <112751504+DM-09@users.noreply.github.com>
2022-12-17 12:49:10 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,31 @@
 var bool = 0
 var be_year = 2022 //현재 년도 -기본 값: 2022
 var af_year = 2023 //새해 년도 -기본 값: 2023
+var is = 0
+
+var full = 'https://user-images.githubusercontent.com/112751504/208221388-7468a028-17da-4eee-a2be-70a200fa0d79.png'
+
+var no = 'https://user-images.githubusercontent.com/112751504/208221578-ebb13ce5-0cf4-4ee3-b9a8-89c2f6a8dac7.png'
+
+var playing = 'https://user-images.githubusercontent.com/112751504/208221987-6bc88e7e-9fff-41e5-88b3-4e8dc2c03fb1.png'
+
+function goGithub() {
+  location.replace('https://github.com/DM-09/CountDown');
+}
+
+function toggleFullScreen() {
+  var btn = document.querySelector("img#fulls")
+  
+  if (!document.fullscreenElement) {
+    document.documentElement.requestFullscreen()
+    btn.src = full
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen()
+      btn.src = no
+    }
+  }
+}
 
 function AutoYear() { //새해 7일 후 자동으로 년도를 바꿔줌
   let date = new Date();
@@ -29,6 +54,18 @@ function Animation() {
   }
 } 
 
+function play() {
+  var playa = document.querySelector("img#playa")
+  
+  if (is == 0) {
+    Animation()
+    playa.src = playing
+    is = 1
+  } else {
+    location.replace('https://codepen.io/DM-09/pen/QWxJrWB');
+  }
+}
+
 function CountDown() {
 	const now = new Date().getTime();
 	var The_Day = new Date(be_year,12,01,00,00,00).getTime();
@@ -51,11 +88,6 @@ function CountDown() {
 		if (hours < 10) { hours = '0' + hours }
 		if (minutes < 10) { minutes = '0' + minutes }
 		if (seconds < 10) { seconds = '0' + seconds }
-    
-    if (seconds == 00) {
-      Animation()
-      bool = 1
-    } //test
 
 		var text = days + ' : ' + hours + ' : ' + minutes + ' : '+ seconds
 		mark.innerHTML = text

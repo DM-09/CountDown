ed91a7625fb5ff57c94fbc194f2fe054197f9205
dongmin <happydm09@naver.com>
2024-01-17 22:22:54 +0900

    Update main.js

--- main.js
+++ main.js
@@ -44,7 +44,6 @@ function CountDown() {
   }
   return text
 }
-
 function RealTimer() {
   var now = new Date()
   var box = [now.getMonth() + 1, now.getDate(), now.getHours(), now.getMinutes(), now.getSeconds(), now.getFullYear()]
@@ -59,21 +58,36 @@ function RealTimer() {
   
   return text
 }
-
 function name(year) {
   var a = "경신임계갑을병정무기"
   var b = "신유술해자축인묘진사오미"
   
   var a2 = "庚辛壬癸甲乙丙丁戊己"
   var b2 = "申酉戌亥子丑寅卯辰巳午未"
-
-  var text = year + '년: ' + a[year % 10] + b[year % 12] + '(' + a2[year % 10] + b2[year % 12] + ')년'
+ 
+  var color = ['흰', '흰', '검은', '검은', '푸른', '푸른', '붉은', '붉은', '황금', '황금']
+  
+  var ani = ['원숭이', '닭', '개', '돼지', '쥐', '소', '호랑이', '토끼', '용', '뱀', '말', '양']
+  
+  var text = `${year + '년: ' + a[year % 10] + b[year % 12] + '(' + a2[year % 10] + b2[year % 12] + ')년'} <br>　${color[year % 10]} ${ani[year % 12]}의 해`
+  
+  if (year % 4 == 0 && year % 100 != 0) {
+    text += ', 윤년'
+  } else if (year % 400 == 0) { text += ', 윤년' }
   
   if (!yearinfo) { return '' } 
   return "　" + text
 }
-
+function UpdateCountDown() {
+  var date = new Date()
+  var year = date.getFullYear()
+  if (date.getMonth() == 0) {
+    if (date.getDate() >= 7) { next_year = year + 1 }
+  } else { next_year = year + 1 }
+  dday = Date.parse(`${next_year}/01/01 00:00:00`);
+}
 function main() {
+  UpdateCountDown()
   var timer = RealTimer()
   var down = CountDown()
   var NAME = name(new Date().getFullYear())
@@ -86,6 +100,7 @@ function main() {
   $("#name").html(NAME)
 }
 
+// Setting Functions
 function ShowFireworks() {
   if (firework) { 
     firework = false
@@ -101,9 +116,9 @@ function ShowYearinfo() {
   else { yearinfo = true }
 }
 function language() {
-  var ko = ['설정', '불꽃놀이 보기', '년도 정보 표시', '개발자 모드 활성화', '언어', '카운트다운 테스트', '-- 개발자 모드 --', '메시지 수정']
-  var en = ['Setting', 'Show fireworks', 'Show year info', 'Enable Dev Mode', 'Language', 'Test Countdown', '-- Dev Mode --', 'Change message']
-  var el = ['m_title', 'm_fw', 'm_yi', 'm_dev', 'lang', 'test', 'm_dla', 'settext']
+  var ko = ['설정', '불꽃놀이 보기', '년도 정보 표시', '개발자 모드 활성화', '언어', '카운트다운 테스트', '-- 개발자 모드 --', '메시지 수정', '년도 정보 테스트']
+  var en = ['Setting', 'Show fireworks', 'Show year info', 'Enable Dev Mode', 'Language', 'Test Countdown', '-- Dev Mode --', 'Change message', 'Year Information Test']
+  var el = ['m_title', 'm_fw', 'm_yi', 'm_dev', 'lang', 'test', 'm_dla', 'settext', 'yearinfotest']
   
   if (lang) { lang = 0 }
   else { lang = 1 }
@@ -117,7 +132,6 @@ function language() {
       $('#'+el[i]).html(en[i])
     }
   }
-  console.log('1')
 }
 function TestCountdown() {
   var now = new Date();
@@ -128,7 +142,7 @@ function Devmode() {
   if (dev) { dev = false }
   else { dev = true }
   
-  var el = ['test', 'm_dla', 'settext']
+  var el = ['test', 'm_dla', 'settext', 'yearinfotest']
   
   if (dev) {
     for (var i=0; i < el.length; i++) {
@@ -145,6 +159,12 @@ function Settext() {
   if (!lang) { text = 'Please enter a message to replace' }
   var input = prompt(text, 'Happy New Year!')
   endText = input
+}
+function TestYearInfo() {
+  var text = '년도 정보를 테스트할 년도를 입력하세요'
+  if (!lang) { text = 'Enter the year for which you want to test year information.' }
+  var input = prompt(text)
+  alert(name(parseInt(input)).replace('<br>', '').replace('　', ''))
 }
 
 language()
